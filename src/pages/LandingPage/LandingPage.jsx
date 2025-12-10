a9871d8ecac50c766477521a226dd9d054e75da1
Ashif <ashifahmd924@gmail.com>
2025-12-10 09:08:31 +0530

    Initial FAAVA frontend upload

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -17,7 +17,7 @@ const LandingPage = () => {
         <Navbar />
     <div className="w-full max-w-7xl mx-auto px-0 md:px-4 lg:px-0">
         <Hero />
-        <Testimonial />
+        <Testimonial /> 
         <Projects />
         <HappyClient />
         <ClientsMentions />
